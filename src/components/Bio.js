78798833e097422ff863d72abe19decb26ac7583
Bobby Hinton <bob.g.hint@gmail.com>
2022-07-21 16:04:19 -0500

    commit tree b6b48aeb4e022f03098d5a65cc69ab550968011a

---
+++ src/components/Bio.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Bio (props) {
+  if(!props.bio) {
+    return null;
+  } else {
+    return (
+      <p>{props.bio}</p>
+    );
+  }
+}
+
+export default Bio; 
